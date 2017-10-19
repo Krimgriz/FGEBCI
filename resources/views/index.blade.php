7aa22bbd065de25c72020350bdce2cdae62dcee9
FGE-HP\FGE <rexar1130@live.com.mx>
2017-10-19 18:35:37 -0500

    avance

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>CompuKami</title>
+    
+    <!--<script src="{{ url('/assets/bootstrap/js/bootstrap.min.js') }}"></script>-->
+    @include('datatable-cdn')
+
+</head>
+<body>
+    <header>
+        @include('partials.navbar')
+    </header>
+
+    <div class="container">
+        
+        
+    </div>
+</body>
+</html>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,65 +1,40 @@
 
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+            <img src="{{ url('/img/FGE-icon.png') }}" width="40" height="40" alt="">
+            
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-<nav class="navbar navbar-default">
-    <div class="container">
-
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="{{url('/')}}">
-                <span class="glyphicon glyphicon-tower" aria-hidden="true"></span>
-                CompuKami
-            </a>
-        </div>
-
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li{{ Request::is('catalog*') && !Request::is('catalog/create')? ' class=active' : ''}}>
-                    <a href="{{url('/catalog')}}">
-                        <span class="glyphicon glyphicon-film" aria-hidden="true"></span>
-                        Catálogo
-                    </a>
-                </li>
-            @if( Auth::check() )
-                <li{{ Request::is('catalog/create') ? ' class=active' : ''}}>
-                    <a href="{{url('/catalog/create')}}">
-                        <span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span>
-                        Nueva película
-                    </a>
-                </li>
-            @endif
-            </ul>
-
-
-            <ul class="nav navbar-nav navbar-right">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
                 
+                
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Busqueda de información
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Persona</a>
+                        <a class="dropdown-item" href="#">Dirección</a>
+                        <a class="dropdown-item" href="#">Vehículo</a>
+                        <a class="dropdown-item" href="#">Número de Carpeta</a>
+                    </div>
+                </li>
 
-            @if(Auth::check())        
-                <li>
-                    <a href="{{url('/logout')}} ">
-                        <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
-                        Cerrar Sesión
-                        
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Generación de estadisticas
                     </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Lugares</a>
+                        <a class="dropdown-item" href="#">Delitos</a>
+                        <a class="dropdown-item" href="#">Internas</a>
+                        <a class="dropdown-item" href="#">Especiales</a>
+                    </div>
                 </li>
-            @else
-                <li>
-                    <a href="{{url('/login')}}">
-                        <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
-                        Login
-                    </a>
-                </li>
-                <li>
-                    <a href="{{ route('register') }}">
-                        Register
-                    </a>
-                </li>
-            @endif
             </ul>
         </div>
-    </div>
-</nav>
+    </nav>
